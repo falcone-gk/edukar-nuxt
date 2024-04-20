0f8c52586a67c1d313b0d4cc31250da96304b1fe
Enzo Falcon <falcone134@gmail.com>
2024-04-20 00:27:59 -0500

    add gap in secitons

--- pages/forum/index.vue
+++ pages/forum/index.vue
@@ -6,34 +6,47 @@
       </template>
       <div>
         <p>Este es el foro EDUKAR, donde podrán compartir y ayudar a otros estudiantes con ejercicios, dudas, consultas
-          y resoluciones sobre problemas matemáticos y de ciencias.<br><span class="text-primary">Regístrate y se parte de
-          esta comunidad estudiantil!!!</span>
+          y resoluciones sobre problemas matemáticos y de ciencias.<br><span class="text-primary">Regístrate y se parte
+            de
+            esta comunidad estudiantil!!!</span>
         </p>
       </div>
     </UCard>
+    <div>
+      <UButton label="Crear nuevo post" icon="i-heroicons-plus-circle-solid" to="/forum/new-post" />
+    </div>
 
     <SkeletonForumPost v-if="pending" />
 
-    <div v-else>
+    <div v-else class="flex flex-col gap-8">
       <UCard v-for="section in sections" :key="'section-' + section.id" :ui="{
-        body: { padding: '' }
-      }">
+      body: { padding: '' }
+    }">
         <template #header>
           <h2 class="text-primary text-xl">
-            <ULink @click="subsectionSelected = 0" class="hover:underline" :to="'/forum/' + section.slug">{{ section.name }}</ULink>
+            <ULink @click="subsectionSelected = 0" class="hover:underline" :to="'/forum/' + section.slug">{{
+      section.name }}
+            </ULink>
           </h2>
         </template>
-        <div class="flex items-center justify-between px-4 sm:px-6 border-b last:border-b-0 dark:border-gray-700 py-4" v-for="subsection in section.subsections" :key="'subsection-' + subsection.id">
+        <div class="flex items-center justify-between px-4 sm:px-6 border-b last:border-b-0 dark:border-gray-700 py-4"
+          v-for="subsection in section.subsections" :key="'subsection-' + subsection.id">
           <div>
-            <ULink @click="subsectionSelected = subsection.id" :to="`/forum/${section.slug}`" class="text-left hover:underline">{{ subsection.name }}</ULink>
+            <ULink @click="subsectionSelected = subsection.id" :to="`/forum/${section.slug}`"
+              class="text-left hover:underline">{{ subsection.name }}</ULink>
           </div>
-          <div class="text-right">
+          <div v-if="subsection.last_post" class="text-right">
             <div>
-              <ULink class="hidden sm:block hover:underline">{{ truncateText(subsection.last_post.title) }}</ULink>
-              <ULink class="text-sm sm:hidden hover:underline">{{ truncateText(subsection.last_post.title, 15) }}</ULink>
+              <ULink class="hidden sm:block hover:underline">
+                {{ truncateText(subsection.last_post.title) }}
+              </ULink>
+              <ULink class="text-sm sm:hidden hover:underline">
+                {{ truncateText(subsection.last_post.title, 15) }}
+              </ULink>
             </div>
             <div class="text-xs">
-              <p><span class="text-primary">{{ subsection.last_post.author }}</span>, {{ subsection.last_post.date }}</p>
+              <p><span class="text-primary">{{ subsection.last_post.author }}</span>, {{ subsection.last_post.date }}
+              </p>
             </div>
           </div>
         </div>
@@ -50,15 +63,15 @@ interface sectionData {
   name: string,
   slug: string,
   subsections: {
-      id: number,
-      name: string,
+    id: number,
+    name: string,
+    slug: string,
+    last_post: {
+      title: string,
       slug: string,
-      last_post: {
-          title: string,
-          slug: string,
-          date: string,
-          author: string,
-      };
+      date: string,
+      author: string,
+    };
   }[];
 }
 
@@ -66,6 +79,9 @@ const subsectionSelected = useState('subsection')
 
 const { data: sections, pending } = await useLazyAsyncData<sectionData[]>(
   'forum',
-  () => useApiFetch<sectionData[]>('/forum/home-forum')
+  () => useApiFetch<sectionData[]>('/forum/home-forum'),
+  {
+    server: false
+  }
 )
 </script>
