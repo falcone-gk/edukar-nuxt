3ceee543059244bb901a7152ffe643b679420a3c
Enzo Falcon <falcone134@gmail.com>
2024-04-20 14:14:53 -0500

    fix some fetch in comments and replies

--- pages/forum/[section]/posts/[slug].vue
+++ pages/forum/[section]/posts/[slug].vue
@@ -2,20 +2,21 @@
   <div class="mx-auto w-full max-w-[900px]">
 
     <UModal prevent-close v-model="isOpen" :ui="{
-        width: 'w-full sm:max-w-[600px] md:max-w-[700px]',
-        ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800',
-        container: 'flex min-h-full items-center justify-center text-center'
-      }">
+      width: 'w-full sm:max-w-[600px] md:max-w-[700px]',
+      ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+      container: 'flex min-h-full items-center justify-center text-center'
+    }">
       <UCard>
         <template #header>
           <div class="flex items-center justify-between">
             <h3 class="text-primary">{{ commentModal.title }}</h3>
-            <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="closeModal" />
+            <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1"
+              @click="closeModal" />
           </div>
         </template>
-  
+
         <TipTap :value="commentModal.body" v-model="commentModal.body" />
-  
+
         <template #footer>
           <UButton @click="sendRequest" label="Enviar" />
         </template>
@@ -28,38 +29,27 @@
 
     <UCard v-else>
       <div>
-        <PostContent
-        @show-text-editor="setupModal('post', 'comment', sendComment, post)"
-        type="post"
-        :title="post.title"
-        :date="post.date"
-        :username="post.author.username"
-        :picture="post.author.picture"
-        :body="post.body"/>
-  
+        <PostContent @show-text-editor="setupModal('post', 'comment', sendComment, post)" type="post"
+          :title="post.title" :date="post.date" :username="post.author.username" :picture="post.author.picture"
+          :body="post.body" />
+
         <UDivider>
           <LogoEdukar />
         </UDivider>
-  
+
         <h2>Comentarios:</h2>
-  
+
         <div class="space-y-4">
           <PostContent v-for="comment in post.comments"
-          @show-text-editor="setupModal('post', 'reply', sendReply, comment, post.id)"
-          @show-update-text-editor="setupModal('update', 'comment', updateComment, comment)"
-          type="comment"
-          :date="comment.date"
-          :username="comment.author.username"
-          :picture="comment.author.picture"
-          :body="comment.body">
+            @show-text-editor="setupModal('post', 'reply', sendReply, comment)"
+            @show-update-text-editor="setupModal('update', 'comment', updateComment, comment, post.id)" type="comment"
+            :date="comment.date" :username="comment.author.username" :picture="comment.author.picture"
+            :body="comment.body">
             <template v-if="comment.replies.length > 0" #replies>
               <PostContent v-for="reply in comment.replies"
-              @show-update-text-editor="setupModal('update', 'reply', updateReply, reply, comment.id)"
-              type="reply"
-              :date="reply.date"
-              :username="reply.author.username"
-              :picture="reply.author.picture"
-              :body="reply.body"/>
+                @show-update-text-editor="setupModal('update', 'reply', updateReply, reply, comment.id)" type="reply"
+                :date="reply.date" :username="reply.author.username" :picture="reply.author.picture"
+                :body="reply.body" />
             </template>
           </PostContent>
         </div>
@@ -96,7 +86,7 @@ const commentModal = reactive<CommentPost>({
   type: null,
   id: null,
   parentId: null,
-  send: () => {}
+  send: () => { }
 })
 
 const route = useRoute()
@@ -121,7 +111,7 @@ const { data: reply, execute: sendReply } = await useLazyAsyncData<PostData>(
   'post-reply',
   () => useApiFetch<PostData>('/forum/replies/', {
     method: 'post',
-    body: { body: commentModal.body, post: commentModal.id }
+    body: { body: commentModal.body, comment: commentModal.id }
   }),
   { server: false, immediate: false }
 )
@@ -153,7 +143,7 @@ const setupModal = (
   callable: () => {},
   data?: Comment | Reply,
   parentId?: number,
-  ) => {
+) => {
 
   if (method === 'post') {
     commentModal.title = 'Nuevo comentario'
