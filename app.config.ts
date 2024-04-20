afa3356e91e082f3bb0816ab32db300b2ad2d877
Enzo Falcon <falcone134@gmail.com>
2024-04-20 00:22:35 -0500

    add notifications

--- app.config.ts
+++ app.config.ts
@@ -7,6 +7,9 @@ export default defineAppConfig({
       variant: {
         solid: 'shadow-sm text-white bg-{color}-500 hover:bg-{color}-600 disabled:bg-{color}-500 dark:bg-{color}-400 dark:hover:bg-{color}-500 dark:disabled:bg-{color}-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-{color}-500 dark:focus-visible:outline-{color}-400'
       }
+    },
+    notifications: {
+      position: 'top-0 right-0',
     }
   }
 })
