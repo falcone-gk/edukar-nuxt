d8d17509f5907f2ec7f3c7b9ca737de6cb8a999a
Enzo Falcon <falcone134@gmail.com>
2024-04-21 11:09:24 -0500

    change grid layout

--- components/skeleton/CardList.vue
+++ components/skeleton/CardList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+  <div class="grid gap-4 auto-cols-auto grid-cols-[repeat(auto-fill,minmax(15rem,1fr))]">
     <UCard v-for="index in 4" :key="`card-skel-${index}`">
       <div class="flex flex-col">
         <USkeleton class="mx-auto h-64 w-full min-w-fit" />
