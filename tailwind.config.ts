a721e6178ec423002d628cbaf9a979642699f241
Enzo Falcon <falcone134@gmail.com>
2024-04-21 11:06:54 -0500

    fix custom style bug

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,21 +13,21 @@ export default <Partial<Config>>{
     extend: {
       colors: {
         'silver-tree': {
-            '50': '#effaf5',
-            '100': '#d8f3e6',
-            '200': '#b4e6d0',
-            '300': '#83d2b5',
-            '400': '#4cb692',
-            '500': '#2d9c7a',
-            '600': '#1e7d61',
-            '700': '#186450',
-            '800': '#155040',
-            '900': '#124236',
-            '950': '#09251f',
+          '50': '#effaf5',
+          '100': '#d8f3e6',
+          '200': '#b4e6d0',
+          '300': '#83d2b5',
+          '400': '#4cb692',
+          '500': '#2d9c7a',
+          '600': '#1e7d61',
+          '700': '#186450',
+          '800': '#155040',
+          '900': '#124236',
+          '950': '#09251f',
         },
         secondary: 'rgb(var(--secondary-color) / <alpha-value>)',
-        'dark-accent-color': 'rgb(var(--dark-accent-color) / <alpha-value>)',
-        'accent-color': 'rgb(var(--accent-color) / <alpha-value>)'
+        'dark-accent': 'rgb(var(--dark-accent-color) / <alpha-value>)',
+        'accent': 'rgb(var(--accent-color) / <alpha-value>)'
       }
     }
   }
