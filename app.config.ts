35bcc387a081ee307d4f82fb3fe9afe4fb7cf55e
Enzo Falcon <falcone134@gmail.com>
2024-04-23 00:13:00 -0500

    change button and notif styles

--- app.config.ts
+++ app.config.ts
@@ -2,14 +2,13 @@ export default defineAppConfig({
   ui: {
     primary: 'silver-tree',
     gray: 'cool',
-    strategy: 'override',
     button: {
       variant: {
-        solid: 'shadow-sm text-white bg-{color}-500 hover:bg-{color}-600 disabled:bg-{color}-500 dark:bg-{color}-400 dark:hover:bg-{color}-500 dark:disabled:bg-{color}-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-{color}-500 dark:focus-visible:outline-{color}-400'
+        solid: 'dark:text-white'
       }
     },
     notifications: {
-      position: 'top-0 right-0',
+      position: 'top-0 bottom-auto',
     }
   }
 })
