192f8c18b5a4361d7468d81f685714468708431d
Enzo Falcon <falcone134@gmail.com>
2024-05-01 20:18:05 -0500

    fix name field and adding new schema

--- schemas/auth.ts
+++ schemas/auth.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 const userRegisterSchema = z.object({
     username: z.string().min(1, { message: 'Campo requerido' }).max(150),
     email: z.string().email({ message: 'Email inválido' }),
-    name: z.string().min(1, { message: 'Campo requerido' }).max(150),
+    first_name: z.string().min(1, { message: 'Campo requerido' }).max(150),
     last_name: z.string().min(1, { message: 'Campo requerido' }).max(150),
     password: z.string().min(1, { message: 'Campo requerido' }),
     re_password: z.string().min(1, { message: 'Campo requerido' }),
@@ -29,4 +29,17 @@ const userLoginSchema = z.object({
     password: z.string().min(1, { message: 'Campo requerido' }),
 })
 
-export { userRegisterSchema, userLoginSchema }
+const userUpdateSchema = z.object({
+    username: z.string().min(1, { message: 'Campo requerido' }).max(150),
+    first_name: z.string().min(1, { message: 'Campo requerido' }).max(150),
+    last_name: z.string().min(1, { message: 'Campo requerido' }).max(150),
+    picture: z.instanceof(File).optional().refine((file) => {
+        if (file !== undefined) {
+            return file.type.split('/')[0] === 'image'
+        }
+        return true
+    }, { message: 'Archivo no es una imagen.' }),
+    about_me: z.string().optional()
+})
+
+export { userRegisterSchema, userLoginSchema, userUpdateSchema }
