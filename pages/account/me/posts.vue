ebaa7f52b81c471eacf44411e826902493500375
Enzo Falcon <falcone134@gmail.com>
2024-05-03 01:06:25 -0500

    refactor post user page

--- pages/account/me/posts.vue
+++ pages/account/me/posts.vue
@@ -7,48 +7,54 @@
       </Typography>
     </template>
 
-    <DataLoading :loading="pending" :data="data" :list="data?.results" empty-message="No hay publicaciones">
-      <template #loading>
-        <SkeletonForumPost />
-      </template>
-      <template #data="{ data: posts }">
-        <div class="flex flex-col gap-4">
-          <UCard v-if="posts.results.length > 0" :ui="{ body: { base: 'flex flex-col gap-4', padding: '' } }">
-            <div
-              class="flex justify-between px-4 py-5 sm:p-6 last:border-b-0 border-b-200 dark:border-b-gray-700 border-b"
-              v-for="post in posts.results">
-              <div>
-                <Typography tag="a" variant="big" color="base" :to="`/forum/${post.section.slug}/posts/${post.slug}`">
-                  {{ post.title }}
-                </Typography>
-              </div>
-              <div>
-                <UButton icon="i-heroicons-pencil-square-solid" color="gray" variant="link"
-                  :to="`/forum/posts/${post.slug}/edit`" />
-                <UButton icon="i-heroicons-trash-solid" color="red" variant="ghost"
-                  @click="deleteCallback(post.slug)" />
-              </div>
-            </div>
-            <template v-if="!pending && data" #footer>
-              <div class="flex justify-center">
-                <UPagination :total="data?.count" :page-count="pageCount" v-model="page" />
-              </div>
-            </template>
-          </UCard>
+    <UTable :loading="pending" :rows="data?.results"
+      :loading-state="{ icon: 'i-heroicons-arrow-path-20-solid', label: 'Cargando...' }"
+      :empty-state="{ icon: 'i-heroicons-circle-stack-20-solid', label: 'No hay publicaciones' }"
+      :progress="{ color: 'primary', animation: 'carousel' }" class="w-full" :columns="columns">
+
+      <template #actions-data="{ row }">
+        <div class="space-x-2">
+          <UTooltip text="Ver post">
+            <UButton color="gray" icon="i-heroicons-arrow-top-right-on-square-solid" size="xs"
+              :to="`/forum/${row.section.slug}/posts/${row.slug}`" />
+          </UTooltip>
+
+          <UTooltip text="Editar post">
+            <UButton size="xs" icon="i-heroicons-pencil-solid" color="gray" :to="`/forum/posts/${row.slug}/edit`" />
+          </UTooltip>
+
+          <UTooltip text="Eliminar post">
+            <UButton icon="i-heroicons-trash-solid" color="red" size="xs" @click="deleteCallback(row.slug)" />
+          </UTooltip>
         </div>
       </template>
-    </DataLoading>
+    </UTable>
+
+    <div v-if="!pending" class="flex justify-between px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
+      <UPagination v-model="page" :page-count="pageCount" :total="data?.count" />
+    </div>
 
   </UCard>
 </template>
 
 <script lang="ts" setup>
+import type { PostData } from '~/types/forum';
+
+type PostPagination = PaginationData<PostData>
 
 const userStore = useUserStore()
+const columns = ref([
+  { key: 'title', label: 'Título' },
+  { key: 'section.name', label: 'Sección' },
+  { key: 'subsection.name', label: 'Subsección' },
+  { key: 'time', label: 'Fecha' },
+  { key: 'actions', label: 'Acciones' }
+])
+
 const page = ref(1)
 const pageCount = ref(5)
 const { data, pending, refresh } = useAsyncData('user-posts',
-  () => useApiFetch('/forum/posts/', {
+  () => useApiFetch<PostPagination>('/forum/posts/', {
     query: {
       username: userStore.user?.username,
       page: page.value,
@@ -58,7 +64,7 @@ const { data, pending, refresh } = useAsyncData('user-posts',
   {
     lazy: true,
     server: false,
-    watch: [page, pageCount]
+    watch: [page]
   }
 )
 
