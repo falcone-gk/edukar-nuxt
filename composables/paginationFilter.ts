c6a6de1679d8658b0ce0804dce06b14fd3f9720c
Enzo Falcon <falcone134@gmail.com>
2024-05-04 01:13:52 -0500

    change pagination filter flow

--- composables/paginationFilter.ts
+++ composables/paginationFilter.ts
@@ -1,34 +1,46 @@
 interface PaginationFilter {
+  key: string
+  size: number
   filters: { [key: string]: string | number | boolean | undefined }
+  url: string
 }
 
-export const usePaginationFilter = ({ filters }: PaginationFilter) => {
+export const usePaginationFilter = <T>({ key, size, filters, url }: PaginationFilter) => {
   const page = ref(1)
-  const userFilters = reactive(filters)
-  const refresh = ref(async () => Promise<any>)
-
-  const setRefresh = (refreshCallback: () => Promise<any>) => {
-    refresh.value = refreshCallback
-  }
-
   const initialUndefined: { [key: string]: undefined } = {}
   Object.keys(filters).forEach(filter => {
     initialUndefined[filter] = undefined
   })
 
-  watch(userFilters, async () => {
+  const { data, pending, refresh } = useAsyncData(
+    key,
+    () => useApiFetch<T>(url, {
+      query: {
+        ...filters,
+        page: page.value,
+        size: size
+      }
+    }),
+    {
+      lazy: true,
+      server: false,
+      watch: [page]
+    }
+  )
+
+  watch(filters, async () => {
     if (page.value !== 1) {
       page.value = 1
     } else {
-      await refresh.value()
+      await refresh()
     }
   })
 
   const clearFilters = async () => {
     // Clear all filters and reset page to 1
-    Object.assign(userFilters, initialUndefined)
+    Object.assign(filters, initialUndefined)
   }
 
-  return { page, clearFilters, setRefresh }
+  return { data, pending, page, clearFilters, refresh }
 
 }
