19ea62dbf70775465ce58b0d37fafe03b0ed4f19
Enzo Falcon <falcone134@gmail.com>
2024-05-06 00:52:02 -0500

    Fix server mismatch in exams fetch

--- components/home/sections/Downloads.vue
+++ components/home/sections/Downloads.vue
@@ -3,14 +3,11 @@
     <p class="text-lg sm:text-xl md:text-2xl lg:text-xl xl:text-2xl">Entra a nuestra galería de exámenes de admisión de
       las diferentes universidades donde podrás descargarlos y practicar lo aprendido.
     </p>
-    <SkeletonCardList v-if="pending" />
-    <ClientOnly v-else>
-      <UCarousel class="w-[256px] md:w-[512px] lg:w-[768px] rounded-lg overflow-hidden" arrows v-slot="{ item }"
-        :items="exams" :ui="{ item: 'basis-full md:basis-1/2 lg:basis-1/3' }">
-        <CardResume :image="item.cover" :title="item.title" class="w-full"
-          @callback="onClick(item.year, item.root.siglas)" />
-      </UCarousel>
-    </ClientOnly>
+    <UCarousel v-if="exams" class="w-[256px] md:w-[512px] lg:w-[768px] rounded-lg overflow-hidden" arrows
+      v-slot="{ item }" :items="exams" :ui="{ item: 'basis-full md:basis-1/2 lg:basis-1/3' }">
+      <CardResume :image="item.cover" :title="item.title" class="w-full"
+        @callback="onClick(item.year, item.root.siglas)" />
+    </UCarousel>
     <UButton to="/downloads">Ir a descargar</UButton>
   </div>
 </template>
@@ -20,12 +17,13 @@ import type { Exams } from '~/types/resultApiTypes'
 
 type ExamPagination = PaginationData<Exams>
 
-const { data: exams, pending } = useLazyAsyncData(
-  'exams', () => useApiFetch<ExamPagination>('/services/exams-list', {
+const { data: exams } = useLazyAsyncData(
+  'exams-home-page', () => useApiFetch<ExamPagination>('/services/exams-list', {
     query: {
       size: 4
     }
   }), {
+  server: false,
   transform: (data: ExamPagination) => {
     const _exams = data.results
     return _exams
