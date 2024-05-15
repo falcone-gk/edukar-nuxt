c1226bd708571183f8b7d7a232acb97ebb3dd544
Enzo Falcon <falcone134@gmail.com>
2024-05-15 18:09:46 -0500

    Change some typos colors and sizes

--- app/components/Typography.vue
+++ app/components/Typography.vue
@@ -38,9 +38,9 @@ const computedTag = computed(() => {
 
 const variantClasses = computed(() => {
   const classLookup = {
-    h1: 'text-4xl',
+    h1: 'text-2xl sm:text-3xl md:text-4xl',
     h2: 'text-3xl',
-    h3: 'text-2xl',
+    h3: 'text-lg sm:text-xl md:text-2xl',
     big: 'text-xl',
     bigger: 'text-[2.50rem] md:text-[3.25rem]',
     body: 'text-base',
@@ -55,7 +55,7 @@ const colorClasses = computed(() => {
     primary: 'text-primary-500 dark:text-primary-400',
     danger: 'text-red-500 dark:text-red-400',
     gray: 'text-gray-500 dark:text-gray-400',
-    white: 'text-white'
+    white: 'text-black dark:text-white'
   }
   return classLookup[props.color as keyof typeof classLookup]
 })
