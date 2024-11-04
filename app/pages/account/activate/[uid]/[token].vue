375d3b499cda39d33268ec4dbe4724b56c966b58
Enzo Falcon <falcone134@gmail.com>
2024-11-03 23:53:35 -0500

    fix token validation

--- app/pages/account/activate/[uid]/[token].vue
+++ app/pages/account/activate/[uid]/[token].vue
@@ -11,48 +11,33 @@
 </template>
 
 <script lang="ts" setup>
-
 definePageMeta({
-  layout: 'centered'
-})
+  layout: "centered",
+});
 
-const route = useRoute()
+const route = useRoute();
 
-const { status } = useEdukarAPI('/account/users/activation/', {
-  method: 'POST',
+const { status } = useEdukarAPI("/account/users/activation/", {
+  method: "POST",
   lazy: true,
-  server: false,
+  // server: false,
   body: {
     uid: route.params.uid,
-    token: route.params.token
+    token: route.params.token,
   },
-})
-
-watch(status, () => {
-  console.log(status.value)
-})
-/* const { pending, error } = await useLazyAsyncData('account-activation',
-  () => useApiFetch('/account/users/activation/', {
-    method: 'post',
-    body: {
-      uid: route.params.uid,
-      token: route.params.token
-    }
-  }),
-  { server: false }
-)
- */
+});
 
 const sucessContent = reactive({
-  title: 'Activación de cuenta satisfactoria',
-  message: 'La cuenta ha sido activada satisfactoriamente. Ahora puedes iniciar sesión.',
-  icon: '/icons/Confetti.svg'
-})
+  title: "Activación de cuenta satisfactoria",
+  message:
+    "La cuenta ha sido activada satisfactoriamente. Ahora puedes iniciar sesión.",
+  icon: "/icons/Confetti.svg",
+});
 
 const errorContent = reactive({
-  title: 'Error en activación de cuenta',
-  message: 'Hubo un error al momento de activar la cuenta. Posiblemente esto se deba a que el link usado para activar la cuenta no es el correcto o este link ya ha sido usado para activar su cuenta.',
-  icon: '/icons/identification.svg'
-})
-
+  title: "Error en activación de cuenta",
+  message:
+    "Hubo un error al momento de activar la cuenta. Posiblemente esto se deba a que el link usado para activar la cuenta no es el correcto o este link ya ha sido usado para activar su cuenta.",
+  icon: "/icons/identification.svg",
+});
 </script>
