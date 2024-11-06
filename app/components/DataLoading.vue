2a4e86ee507d6ea5b4bc96e198d99c8d3202253f
Enzo Falcon <falcone134@gmail.com>
2024-11-05 22:41:30 -0500

    Change data loading logic

--- app/components/DataLoading.vue
+++ app/components/DataLoading.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <!--Loading Components section-->
+    <!-- Loading Components section -->
     <div v-if="props.loading">
       <slot name="loading">
         <DataLoadingTransition :message="props.loadingMessage" />
@@ -8,33 +8,39 @@
     </div>
 
     <div v-else>
-      <!--Error section-->
-      <div v-if="props.data === null">
+      <!-- Error section -->
+      <div v-if="!props.data">
         <slot name="error">
           <DataError />
         </slot>
       </div>
 
-      <!--Data section-->
+      <!-- Data section -->
       <div v-else>
-        <div v-if="(Array.isArray(props.data) && props.data.length === 0) || (props.list && props.list.length === 0)">
+        <div v-if="isDataEmpty">
           <DataEmpty :message="props.emptyMessage" />
         </div>
         <div v-else>
-          <slot name="data" :data="props.data"></slot>
+          <slot name="data" :data="props.data" />
         </div>
       </div>
-
     </div>
   </div>
 </template>
 
 <script lang="ts" setup generic="T, E">
 const props = defineProps<{
-  loading?: boolean,
-  data: T | null,
-  list?: E[] | null,
-  emptyMessage?: string
-  loadingMessage?: string
-}>()
+  loading?: boolean;
+  data: T | null;
+  list?: E[] | null;
+  emptyMessage?: string;
+  loadingMessage?: string;
+}>();
+
+const isDataEmpty = computed(() => {
+  return (
+    (Array.isArray(props.data) && props.data.length === 0) ||
+    (props.list && props.list.length === 0)
+  );
+});
 </script>
