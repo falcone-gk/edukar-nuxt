4d3eff4574969b4a8347e5f1cda42a1cb40c5cfb
Enzo Falcon <falcone134@gmail.com>
2024-11-08 21:17:35 -0500

    Add aria-label to improve access

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -1,27 +1,29 @@
 <script setup lang="ts">
-const userStore = useUserStore()
-const colorMode = useColorMode()
+const userStore = useUserStore();
+const colorMode = useColorMode();
 
-const selected = ref<boolean>(colorMode.preference === 'dark' ? true : false)
+const selected = ref<boolean>(colorMode.preference === "dark" ? true : false);
 
 const toggleColorMode = () => {
   const colorSwitcher = {
-    light: 'dark',
-    dark: 'light'
-  }
-  colorMode.preference = colorSwitcher[colorMode.value as 'light' | 'dark']
-}
+    light: "dark",
+    dark: "light",
+  };
+  colorMode.preference = colorSwitcher[colorMode.value as "light" | "dark"];
+};
 
 const iconColorMode = computed(() => {
-  return colorMode.value === 'dark' ? 'i-heroicons-moon-solid' : 'i-heroicons-sun-solid'
-})
+  return colorMode.value === "dark"
+    ? "i-heroicons-moon-solid"
+    : "i-heroicons-sun-solid";
+});
 
 const links = [
-  { text: 'Inicio', path: '/', name: 'home', id: 1 },
-  { text: 'Cursos', path: '/courses', name: 'course', id: 2 },
-  { text: 'Foro', path: '/forum', name: 'forum', id: 3 },
-  { text: 'Descargas', path: '/downloads/exams', name: 'download', id: 4 },
-]
+  { text: "Inicio", path: "/", name: "home", id: 1 },
+  { text: "Cursos", path: "/courses", name: "course", id: 2 },
+  { text: "Foro", path: "/forum", name: "forum", id: 3 },
+  { text: "Descargas", path: "/downloads/exams", name: "download", id: 4 },
+];
 
 /* const darkModeItem = [
   { icon: 'i-heroicons-moon-solid', label: 'Dark mode', slot: 'theme' }
@@ -29,21 +31,39 @@ const links = [
 
 const menuItems = [
   [
-    { icon: 'i-heroicons-user-solid', label: 'Ver perfil', click: () => navigateTo('/account/me') },
-    { icon: 'i-heroicons-chat-bubble-left-right-solid', label: 'Mis publicaciones', click: () => navigateTo('/account/me/posts') },
-    { icon: 'i-heroicons-envelope-solid', label: 'Notificaciones', click: () => navigateTo('/account/me/notifications') },
+    {
+      icon: "i-heroicons-user-solid",
+      label: "Ver perfil",
+      click: () => navigateTo("/account/me"),
+    },
+    {
+      icon: "i-heroicons-chat-bubble-left-right-solid",
+      label: "Mis publicaciones",
+      click: () => navigateTo("/account/me/posts"),
+    },
+    {
+      icon: "i-heroicons-envelope-solid",
+      label: "Notificaciones",
+      click: () => navigateTo("/account/me/notifications"),
+    },
   ],
   [
-    { icon: 'i-heroicons-arrow-right-end-on-rectangle-solid', label: 'Cerrar sesión', click: userStore.logout }
-  ]
-]
+    {
+      icon: "i-heroicons-arrow-right-end-on-rectangle-solid",
+      label: "Cerrar sesión",
+      click: userStore.logout,
+    },
+  ],
+];
 </script>
 
 <template>
-  <header class="sticky top-0 px-4 h-[var(--header-height)] hidden md:block shadow-xl z-10 bg-white dark:bg-secondary">
+  <header
+    class="sticky top-0 px-4 h-[var(--header-height)] hidden md:block shadow-xl z-10 bg-white dark:bg-secondary"
+  >
     <div class="flex justify-between h-full items-stretch">
       <div class="flex my-auto">
-        <NuxtLink to="/">
+        <NuxtLink aria-label="Ir a la página de inicio de Edukar" to="/">
           <LogoEdukar />
         </NuxtLink>
       </div>
@@ -51,32 +71,60 @@ const menuItems = [
         <ul class="h-full">
           <li
             class="inline-flex w-full h-12 justify-center items-center md:h-full md:w-24 [&>a]:w-full [&>a]:h-full [&>a]:grid [&>a]:place-items-center"
-            v-for="link in links" :key="link.id">
-            <ULink :to="link.path" active-class="h-full w-full border-b-2 border-b-primary"
-              inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">{{
-                link.text }}
+            v-for="link in links"
+            :key="link.id"
+          >
+            <ULink
+              :to="link.path"
+              active-class="h-full w-full border-b-2 border-b-primary"
+              inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
+              >{{ link.text }}
             </ULink>
           </li>
         </ul>
       </nav>
       <div class="flex flex-grow-0 my-auto gap-4">
-        <UButton v-if="!userStore.isLogged" to="/login" type="button" label="Iniciar sesión" />
-        <UButton v-if="!userStore.isLogged" to="/signup" type="button" variant="outline" label="Registrarse" />
+        <UButton
+          v-if="!userStore.isLogged"
+          to="/login"
+          type="button"
+          label="Iniciar sesión"
+        />
+        <UButton
+          v-if="!userStore.isLogged"
+          to="/signup"
+          type="button"
+          variant="outline"
+          label="Registrarse"
+        />
         <div>
           <ColorScheme>
-            <UButton :icon="iconColorMode" color="gray" variant="ghost" @click="toggleColorMode" />
+            <UButton
+              aria-label="modo-oscuro"
+              :icon="iconColorMode"
+              color="gray"
+              variant="ghost"
+              @click="toggleColorMode"
+            />
           </ColorScheme>
         </div>
         <div v-if="userStore.user" class="flex gap-2">
           <UDropdown :items="menuItems" :popper="{ placement: 'bottom-start' }">
             <template #theme="{ item }">
-              <UIcon class="flex-shrink-0 w-5 h-5 text-gray-400 dark:text-gray-500" :name="item.icon" />
+              <UIcon
+                class="flex-shrink-0 w-5 h-5 text-gray-400 dark:text-gray-500"
+                :name="item.icon"
+              />
               <span class="truncate">{{ item.label }}</span>
               <div class="flex items-center ml-auto">
                 <UToggle v-model="selected" @click="toggleColorMode" />
               </div>
             </template>
-            <UAvatar img-class="object-cover" :src="userStore.getPicturePath()" alt="user picture" />
+            <UAvatar
+              img-class="object-cover"
+              :src="userStore.getPicturePath()"
+              alt="user picture"
+            />
           </UDropdown>
         </div>
       </div>
--- app/components/Sidebar.vue
+++ app/components/Sidebar.vue
@@ -1,35 +1,57 @@
 <template>
-  <header class="sticky top-0 z-10 h-16 p-4 shadow-xl md:hidden bg-white dark:bg-secondary">
-    <div class="flex  justify-between items-center h-full">
+  <header
+    class="sticky top-0 z-10 h-16 p-4 shadow-xl md:hidden bg-white dark:bg-secondary"
+  >
+    <div class="flex justify-between items-center h-full">
       <div class="flex my-auto">
         <NuxtLink to="/">
           <LogoEdukar />
         </NuxtLink>
       </div>
-      <UButton v-show="!isOpen" icon="i-heroicons-bars-3-20-solid" color="primary" variant="link"
-        @click="isOpen = true" />
+      <UButton
+        v-show="!isOpen"
+        aria-label="Mostra sidebar"
+        icon="i-heroicons-bars-3-20-solid"
+        color="primary"
+        variant="link"
+        @click="isOpen = true"
+      />
     </div>
   </header>
-  <USlideover class="md:hidden" v-model="isOpen" side="left" :ui="{
-    width: 'relative w-5/6 max-w-md',
-    base: ''
-  }">
-    <UCard class="h-full" :ui="{ base: 'flex flex-col', footer: { base: 'mt-auto' } }">
+  <USlideover
+    class="md:hidden"
+    v-model="isOpen"
+    side="left"
+    :ui="{
+      width: 'relative w-5/6 max-w-md',
+      base: '',
+    }"
+  >
+    <UCard
+      class="h-full"
+      :ui="{ base: 'flex flex-col', footer: { base: 'mt-auto' } }"
+    >
       <template v-if="userStore.user" #header>
         <ul>
           <li>
             <div class="flex justify-between px-2.5 py-1.5">
               <div class="flex items-center gap-1.5">
-                <UAvatar img-class="object-cover" :src="userStore.getPicturePath()" />
+                <UAvatar
+                  img-class="object-cover"
+                  :src="userStore.getPicturePath()"
+                />
                 <span>{{ userStore.user.username }}</span>
               </div>
             </div>
           </li>
         </ul>
       </template>
-      <UVerticalNavigation :links="links" :ui="{
-        active: 'bg-primary dark:before:bg-primary-500',
-      }">
+      <UVerticalNavigation
+        :links="links"
+        :ui="{
+          active: 'bg-primary dark:before:bg-primary-500',
+        }"
+      >
         <template #badge="{ link }">
           <div v-if="link.input" class="flex items-center ml-auto">
             <UToggle v-model="selected" @click="toggleColorMode" />
@@ -37,67 +59,105 @@
         </template>
       </UVerticalNavigation>
       <template v-if="userStore.isLogged" #footer>
-        <UButton icon="i-heroicons-arrow-right-end-on-rectangle-solid" label="Cerrar sesión" variant="ghost"
-          @click="userStore.logout" />
+        <UButton
+          icon="i-heroicons-arrow-right-end-on-rectangle-solid"
+          label="Cerrar sesión"
+          variant="ghost"
+          @click="userStore.logout"
+        />
       </template>
     </UCard>
   </USlideover>
 </template>
 
 <script lang="ts" setup>
-const userStore = useUserStore()
-const colorMode = useColorMode()
-const selected = ref<boolean>(colorMode.preference === 'dark' ? true : false)
+const userStore = useUserStore();
+const colorMode = useColorMode();
+const selected = ref<boolean>(colorMode.preference === "dark" ? true : false);
 
 const toggleColorMode = () => {
   const colorSwitcher = {
-    light: 'dark',
-    dark: 'light'
-  }
-  colorMode.preference = colorSwitcher[colorMode.value as 'light' | 'dark']
-}
+    light: "dark",
+    dark: "light",
+  };
+  colorMode.preference = colorSwitcher[colorMode.value as "light" | "dark"];
+};
 
-const isOpen = ref(false)
+const isOpen = ref(false);
 
 const userLinks = [
-  { label: 'Iniciar sesión', to: '/login', icon: 'i-heroicons-user-solid', click: () => isOpen.value = false },
-  { label: 'Registrarse', to: '/signup', icon: 'i-heroicons-user-plus-solid', click: () => isOpen.value = false }
-]
+  {
+    label: "Iniciar sesión",
+    to: "/login",
+    icon: "i-heroicons-user-solid",
+    click: () => (isOpen.value = false),
+  },
+  {
+    label: "Registrarse",
+    to: "/signup",
+    icon: "i-heroicons-user-plus-solid",
+    click: () => (isOpen.value = false),
+  },
+];
 
-const userLoggedLinks = [{
-  label: 'Perfil',
-  icon: 'i-heroicons-user-solid',
-  to: '/account/me',
-  exact: true,
-  click: () => isOpen.value = false
-}, {
-  label: 'Mis Publicaciones',
-  icon: 'i-heroicons-chat-bubble-left-right-solid',
-  to: '/account/me/posts',
-  exact: true,
-  click: () => isOpen.value = false
-}, {
-  label: 'Notificaciones',
-  icon: 'i-heroicons-envelope-solid',
-  to: '/account/me/notifications',
-  exact: true,
-  click: () => isOpen.value = false
-}]
+const userLoggedLinks = [
+  {
+    label: "Perfil",
+    icon: "i-heroicons-user-solid",
+    to: "/account/me",
+    exact: true,
+    click: () => (isOpen.value = false),
+  },
+  {
+    label: "Mis Publicaciones",
+    icon: "i-heroicons-chat-bubble-left-right-solid",
+    to: "/account/me/posts",
+    exact: true,
+    click: () => (isOpen.value = false),
+  },
+  {
+    label: "Notificaciones",
+    icon: "i-heroicons-envelope-solid",
+    to: "/account/me/notifications",
+    exact: true,
+    click: () => (isOpen.value = false),
+  },
+];
 
 const navLinks = [
-  { label: 'Inicio', to: '/', icon: 'i-heroicons-home', click: () => isOpen.value = false },
-  { label: 'Cursos', to: '/courses', icon: 'i-heroicons-book-open-20-solid', click: () => isOpen.value = false },
-  { label: 'Foro', to: '/forum', icon: 'i-heroicons-chat-bubble-left-right-20-solid', click: () => isOpen.value = false },
-  { label: 'Descargas', to: '/downloads/exams', icon: 'i-heroicons-arrow-down-circle-20-solid', click: () => isOpen.value = false },
-]
+  {
+    label: "Inicio",
+    to: "/",
+    icon: "i-heroicons-home",
+    click: () => (isOpen.value = false),
+  },
+  {
+    label: "Cursos",
+    to: "/courses",
+    icon: "i-heroicons-book-open-20-solid",
+    click: () => (isOpen.value = false),
+  },
+  {
+    label: "Foro",
+    to: "/forum",
+    icon: "i-heroicons-chat-bubble-left-right-20-solid",
+    click: () => (isOpen.value = false),
+  },
+  {
+    label: "Descargas",
+    to: "/downloads/exams",
+    icon: "i-heroicons-arrow-down-circle-20-solid",
+    click: () => (isOpen.value = false),
+  },
+];
 
 const darkMode = [
-  { label: 'Dark mode', icon: 'i-heroicons-moon-solid', input: true }
-]
+  { label: "Dark mode", icon: "i-heroicons-moon-solid", input: true },
+];
 
 const links = computed(() => [
   navLinks,
   userStore.isLogged ? userLoggedLinks : userLinks,
-  darkMode
-])
+  darkMode,
+]);
 </script>
