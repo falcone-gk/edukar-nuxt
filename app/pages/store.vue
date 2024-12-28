d6a3f27c4eac0a75abeeb2871b347d9d8463c40f
Enzo Falcon <falcone134@gmail.com>
2024-12-28 03:15:39 -0500

    Add message in store view

--- app/pages/store.vue
+++ app/pages/store.vue
@@ -1,3 +1,10 @@
 <template>
-  <NuxtPage />
+  <div class="relative">
+    <div
+      class="sticky top-12 bg-red-100 text-red-700 dark:bg-red-600 dark:text-red-100 text-center py-2 font-bold shadow-md z-50 -translate-y-10"
+    >
+      La tienda estará disponible próximamente
+    </div>
+    <NuxtPage />
+  </div>
 </template>
